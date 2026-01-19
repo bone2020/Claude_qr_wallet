189bcea49ca775235ffc39f46c774e965d8b51be
qrW_C <bonstrahe@gmail.com>
2026-01-19 12:18:40 +0400

    fix: Add packaging excludes to resolve duplicate META-INF files

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -46,6 +46,23 @@ android {
             )
         }
     }
+
+    packaging {
+        resources {
+            excludes += listOf(
+                "META-INF/DEPENDENCIES",
+                "META-INF/LICENSE",
+                "META-INF/LICENSE.txt",
+                "META-INF/license.txt",
+                "META-INF/NOTICE",
+                "META-INF/NOTICE.txt",
+                "META-INF/notice.txt",
+                "META-INF/ASL2.0",
+                "META-INF/*.kotlin_module",
+                "META-INF/versions/9/OSGI-INF/MANIFEST.MF"
+            )
+        }
+    }
 }
 
 flutter {
