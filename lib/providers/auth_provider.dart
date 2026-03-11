dd1b0e7290fe1d935ec2dedee0d96b1b380c47ed
qrW_C <bonstrahe@gmail.com>
2026-03-11 17:51:13 +0400

    Fix: add missing debugPrint import in auth_provider

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
+import 'package:flutter/foundation.dart';
 import '../core/services/services.dart';
 import '../core/services/push_notification_service.dart';
 import '../models/user_model.dart';
